ba51eb150bb33c778407fa6f83f021332aa88ad1
alyssahellrung <alyssahellrung@gmail.com>
2021-02-08 11:07:54 -0800

    added combine button, started event handlers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var list1 = ["apples", "Bananas", "rice", "crackers", "milk"];
 var list2 = ["kiwi", "milk", "Bread", "bananas", "Rice"];
 var mergedList = [];
+const item = document.getElementById("item");
 
 function listMerge(list1, list2) {
   var lowerlist1 = list1.map(item => item.toLowerCase());
@@ -13,3 +14,20 @@ function listMerge(list1, list2) {
   return(mergedList);  
 }
 console.log(listMerge(list1, list2));
+
+function addToList(item) {
+   list.push(item);
+}
+
+function keydownHandler(event) {
+  if (event.keyCode === 13) {
+  console.log("Enter key pressed");
+    addToList(item);
+  }
+}
+
+window.addEventListener("keydown", keydownHandler, false);
+
+const combine = document.querySelector("#combine");
+combine.addEventListener("click", listMerge, false);
+
